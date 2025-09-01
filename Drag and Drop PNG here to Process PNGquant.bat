b8c45383a285ee456768bff303b45ef624421295
Ingo_Igel <Ingo_Igel@noreply.codeberg.org>
2025-09-01 15:48:59 +0200

    Added counter and echos

--- Drag and Drop PNG here to Process PNGquant.bat
+++ Drag and Drop PNG here to Process PNGquant.bat
@@ -1,13 +1,21 @@
 @echo off
 
 set path=%~d0%~p0
+set /a counter=1
 
 :start
 
-"%path%/pngquant/pngquant.exe" --quality=75-100 --ext=.png --force --skip-if-larger --speed 1 --strip --verbose %1
+echo -start processing PNG no. %counter%
+
+echo --start PNGquant
+"%path%/pngquant/pngquant.exe" --quality=75-100 --ext=.png --force --skip-if-larger --speed 1 --strip --verbose  %1
+echo --finished PNGquant
+
+echo -finished processing PNG no. %counter%
+set /a counter+=1
 
 shift
 if NOT x%1==x goto start
 
-echo "All done!"
+echo !!All done!!
 pause
--- Drag and Drop PNG here to process oxipng.bat
+++ Drag and Drop PNG here to process oxipng.bat
@@ -1,13 +1,21 @@
 @echo off
 
 set path=%~d0%~p0
+set /a counter=1
 
 :start
 
-"%path%/oxipng/oxipng.exe" -o max --strip safe %1
+echo -start processing PNG no. %counter%
+
+echo --start oxiPNG
+"%path%/oxipng/oxipng.exe" -o max --strip safe --preserve %1
+echo --finished oxiPNG
+
+echo -finished processing PNG no. %counter%
+set /a counter+=1
 
 shift
 if NOT x%1==x goto start
 
-echo "All done!"
+echo !!All done!!
 pause
